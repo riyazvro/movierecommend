0ad21bbd3083ed49de23a7271f443a31ca889e18
riyazvro <riyaz.shaikit@gmail.com>
2025-08-27 17:31:36 +0530

    final commit

--- .next/server/app/api/movieoftheday/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/movieoftheday/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_dist_c8de643d._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__a6204271._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/movieoftheday/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/movieoftheday/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/movieoftheday/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/static/chunks/app_favorites_page_bb1306cc.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_16a5d0b6._.js"
-],
-    source: "dynamic"
-});
